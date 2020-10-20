c89ef4b1123b902576c20036d10c0cd46456fe9e
Francisco Huacho <jhersonhuacho@gmail.com>
2020-10-20 06:27:35 -0500

    1-Comenzando con React y 2-JSX

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
+import Curso from './Curso';
+import './styles/styles.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+// -> Primera Forma
+//const App = () => <h1>Hola Mundo desde mi archivo App.js</h1>;
+
+const App = () => (
+  <Fragment>
+    <div className="main-banner img-container l-section" id="main-banner">
+      <div className="ed-grid lg-grid-6">
+        <div className="lg-cols-4 lg-x-2">
+          <img className="main-banner__img" src="https://images.unsplash.com/photo-1477959858617-67f85cf4f1df?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max" alt="banner" />
+          <div className="main-banner__data s-center">
+            <p className="t2 s-mb-0">Curso de EDTeam</p>
+            <p> Tu futuro te está esperando</p>
+            <a href="#" className="button">Botón del banner</a>
+          </div>
+        </div>
+      </div>
     </div>
-  );
-}
+
+    <div className="ed-grid m-grid-3">
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+    </div>
+  </Fragment>
+);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const root = document.getElementById('root');
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// -> Sin JSX
+//const elemento = React.createElement(componente,propiedades,children);
+//const elemento = React.createElement("h1", { className: "saludo" }, "Hola Mundo");
+
+// -> Con JSX
+//const elemento = <h1 className="saludo">Hola Mundo</h1>;
+
+//ReactDOM.render( elemento, root);
+
+ReactDOM.render(<App />, root);
+
+
